3562df49e5acd2979b1d9e5d8a2329c0874de1df
Juan Hidalgo Puertas <jhidalgop@icloud.com>
2016-06-14 23:47:48 +0200

    Initial Commit

---
+++ Pizza/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Pizza
+//
+//  Created by Juan Hidalgo Puertas on 14/6/16.
+//  Copyright © 2016 Juan Hidalgo Puertas. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
